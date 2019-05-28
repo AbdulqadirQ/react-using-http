bd040a18f5fa110b4290841f3a36260da3059567
abdul.qasim <abdul.qasim@baesystems.com>
2019-05-28 09:09:12 +0100

    an example of transforming an http response - limits response and adds an author field

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -12,15 +12,22 @@ class Blog extends Component {
     }
 
     componentDidMount () {
-        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max' // additional (hard-coded) author property since there's no back-end
+                    }
+                })
+                this.setState({posts: updatedPosts});
                 // console.log(response);
             });
     }
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />;
+            return <Post key={post.id} title={post.title} author={post.author}/>;
         });
 
         return (
